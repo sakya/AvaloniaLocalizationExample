3d8dd8e57938107188b4234797292c3cb7559468
Paolo Iommarini <paolo.iommarini@live.com>
2022-12-30 12:01:30 +0100

    Iinital commit

---
+++ AvaloniaLocalizationExample/MainWindow.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaLocalizationExample
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OnLanguageChanged(object sender, SelectionChangedEventArgs args)
+        {
+            var cb = sender as ComboBox;
+            var language = cb.SelectedIndex == 0 ? "en-US" : "it-IT";
+            Localizer.Localizer.Instance.LoadLanguage(language);
+        } // OnLanguageChanged
+
+    }
+}
